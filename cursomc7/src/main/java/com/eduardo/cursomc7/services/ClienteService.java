c533e23a9469ca90ef70f4a1b712cbb6c3fc850a
eduardovalentim7 <eduardovalentim79@gmail.com>
2018-07-24 09:40:53 -0300

    endpoint cliente

---
+++ cursomc7/src/main/java/com/eduardo/cursomc7/services/ClienteService.java
@@ -0,0 +1,28 @@
+package com.eduardo.cursomc7.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.cursomc7.domain.Cliente;
+import com.eduardo.cursomc7.repositories.ClienteRepository;
+import com.eduardo.cursomc7.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if(obj==null) {
+			throw new ObjectNotFoundException("Objeto nao Encontrado! Id " + id 
+					+ ",Tipo: " + Cliente.class.getName());
+		}
+		
+		return obj;
+		
+	}
+	
+
+}
